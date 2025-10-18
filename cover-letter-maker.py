7e94de0e1d8b939ef56bd5d86c39f4c470f91450
Mehul Tandale <mehultandale@gmail.com>
2025-10-18 13:12:48 +0530

    Made script working

--- cover-letter-maker.py
+++ cover-letter-maker.py
@@ -1,11 +1,11 @@
 import pdfkit
 
 # From HTML string
-html_content = "<h1>Hello, PDF!</h1><p>This is a test.</p>"
-pdfkit.from_string(html_content, "output.pdf")
+# html_content = "<h1>Hello, PDF!</h1><p>This is a test.</p>"
+# pdfkit.from_string(html_content, "output.pdf")
 
 # From HTML file
-# pdfkit.from_file("example.html", "output.pdf")
+pdfkit.from_file("example.html", "output.pdf")
 
 # From URL
 # pdfkit.from_url("https://example.com", "output.pdf")
